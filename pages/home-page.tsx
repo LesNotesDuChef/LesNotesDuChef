8274b0e7f10481fa90023ffcd24d9ff2b594e7ad
bastien-riado <bastien.riado@gmail.com>
2022-05-10 23:19:35 +0200

    mdr découverte donc pas grand chose

--- pages/home-page.tsx
+++ pages/home-page.tsx
@@ -1,9 +1,18 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { setStatusBarHidden, setStatusBarStyle } from 'expo-status-bar';
+import { SafeAreaView, ScrollView, StatusBar, Modal, StyleSheet, Text, View } from 'react-native';
 import HomePageTile from './home-page-tile';
 
 export default function HomePage() {
   return (
     <SafeAreaView style={styles.container}>
+ {/*}     <StatusBar  style={styles.statusbar}>
+        animated={true}
+        backgroundColor="#61dafb"
+      </StatusBar>
+{/*      <Modal style={styles.modal}>
+        animationType='slide'
+        transparent={true}
+      </Modal>*/}
       <ScrollView style={styles.scrollView}>
           <HomePageTile></HomePageTile>
           <HomePageTile></HomePageTile>
@@ -23,4 +32,14 @@ const styles = StyleSheet.create({
   scrollView: {
     marginHorizontal: 10,
   },
+  modal: {
+    margin: 20,
+    backgroundColor: "black",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+  },
+
 });
+
