50ca1a9264fc379b9eb7d013c42fca6ba249ddf9
Tanguy Poinson <92086672+tanguyPoinson@users.noreply.github.com>
2022-05-11 00:00:58 +0200

    1 user story home page (#3)
    * premier ajout
    
    * utilisation de données depuis un fichier json
    
    * mdr découverte donc pas grand chose
    
    * redo
    
    * reformattage
    
    * Nouveau Dossier
    
    * changement de nom
    
    Co-authored-by: Tanguy Poinson <105313123+LesNotesDuChef@users.noreply.github.com>
    Co-authored-by: bastien-riado <bastien.riado@gmail.com>

---
+++ pages/home-page/home-page-tile.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const HomePageTile = (props: any) => {
+    return(
+        <TouchableOpacity style={styles.container}>
+            <Text style={styles.title}>
+                {props.titre}
+            </Text>
+            <Text style={styles.text}>
+                {props.description}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+export default HomePageTile;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#ededed",
+      border: 2,
+      borderRadius: 5,
+      marginBottom: 5,
+    },
+    title: {
+      fontSize: 18,
+    },
+    text: {
+      fontSize: 14,
+    },
+  });
