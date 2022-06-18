9c16650b80b25d217ea7bd5b7bc8142dd583a4d0
Tanguy Poinson <tanguy.poinson@grenoble-inp.org>
2022-06-18 16:44:23 +0200

    premier jet avant de changer d'us

--- pages/RecipeView.tsx
+++ pages/RecipeView.tsx
@@ -1,7 +1,34 @@
-import { View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 
-const RecipeView = () => {
+/**
+ * Vue sur une recette
+ * 
+ * @returns JSX sur une vue de recette
+ * 
+ * @author Tanguy Poinson
+ */
+const RecipeView = (props: any) => {
     return(<View>
-        
-    </View>)
+        <Text>props.titre</Text>
+    </View>);
 } 
+
+/**
+ * CSS de la home page
+ */
+ const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        paddingLeft: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        fontSize: 28,
+    },
+    scrollView: {
+        marginHorizontal: 5,
+    },
+});
+
+export default RecipeView;
